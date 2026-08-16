6ef9364cef30c12a56c42293c0284194d514962f
mk-prymak <mark.prymak@gmail.com>
2026-08-16 23:16:13 +0200

    Structure creation & Generall tasks (1)
    I have created the desired structure for the further work; and started the Generall Tasks (1a-d)

---
+++ SoSe26_Case_Study_App_Group_38.py
@@ -0,0 +1,25 @@
+"""
+SoSe26 Case Study App – Group 38
+
+Placeholder web application. Will load only:
+    Data/SoSe26_Case_Study_finalData_Group_38.csv
+
+Design constraints (to implement later):
+- main colour: light blue
+- font: Source Sans Pro
+- logo: own or Department of Quality Science
+- target audience: management / stakeholders
+"""
+
+# TODO: choose Dash and/or Streamlit and implement the app
+
+
+def main() -> None:
+    raise NotImplementedError(
+        "Web app not implemented yet. "
+        "Final dataset: Data/SoSe26_Case_Study_finalData_Group_38.csv"
+    )
+
+
+if __name__ == "__main__":
+    main()
